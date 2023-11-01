4858253b585f7ba6bc41ec32ff104d820b7a9847
Andrei Siclovan <andreisiclovan@yahoo.com>
2023-11-01 10:39:27 +0200

    Initial Commit

---
+++ SurveyApp/SurveyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SurveyAppApp.swift
+//  SurveyApp
+//
+//  Created by Andrei Siclovan on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SurveyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
